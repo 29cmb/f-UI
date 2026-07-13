27634595bdef16def592ce47827caa9d7b706743
29cmb <devcmb@outlook.com>
2026-07-13 15:17:03 -0400

    fix: :bug: Fix even more problems with jitpack

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("jvm")
     id("com.gradleup.shadow") version "9.5.1"
     kotlin("plugin.serialization") version "2.4.0"
+    `maven-publish`
 }
 
 dependencies {
@@ -17,9 +18,6 @@ publishing {
     publications {
         register<MavenPublication>("release") {
             artifact(tasks.shadowJar)
-            groupId = project.group.toString()
-            artifactId = "f-ui"
-            version = project.version.toString()
         }
     }
 }
